fc0501c8ee67c45bcc25ae47a8418ee6d84502c4
Yuta Yotsumoto <y.2000529@gmail.com>
2021-07-12 15:11:33 +0900

    基盤作成

---
+++ src/Screen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {SafeAreaView, View, Text, StyleSheet} from 'react-native';
+
+const Screen = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>画面遷移やってみよう</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default Screen;
