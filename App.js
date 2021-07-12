e025266cf20b31e5de6bc20f418dee2f8c81d0cc
Yuta Yotsumoto <y.2000529@gmail.com>
2021-07-12 19:51:48 +0900

    ReactNavigation / Library / pod インストール import　react-native-gesture-handler

--- App.js
+++ App.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+
+import {} from 'react-native';
 import Screen from './src/Screen';
 
 const App = () => {
-  return <Screen />;
+  return <NavigationContainer>{<Screen />}</NavigationContainer>;
 };
 
 export default App;
