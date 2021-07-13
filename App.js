895cad4d517ca10a4cbb9c7fd2bcc75f1a5e2edb
Yuta Yotsumoto <y.2000529@gmail.com>
2021-07-13 09:56:12 +0900

    react-native-gesture-handlerライブラリを改善

--- App.js
+++ App.js
@@ -2,11 +2,14 @@ import 'react-native-gesture-handler';
 import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 
-import {} from 'react-native';
 import Screen from './src/Screen';
 
 const App = () => {
-  return <NavigationContainer>{<Screen />}</NavigationContainer>;
+  return (
+    <NavigationContainer>
+      <Screen />
+    </NavigationContainer>
+  );
 };
 
 export default App;
