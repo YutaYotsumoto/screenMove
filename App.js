9fc4a7661f1722ce370cdfd875cb0413f210ee3a
Yuta Yotsumoto <y.2000529@gmail.com>
2021-07-14 14:58:55 +0900

    StackNavigationのデフォルト作成

--- App.js
+++ App.js
@@ -1,15 +1,40 @@
 import 'react-native-gesture-handler';
 import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
+//①インポート
+import {createStackNavigator} from '@react-navigation/stack';
 
-import Screen from './src/Screen';
+import {SafeAreaView, Text, StyleSheet} from 'react-native';
 
+//②画面表示するための関数コンポネントを作成
+const ProjectScreen = () => {
+  return (
+    <SafeAreaView style={styles.HomeScreen}>
+      <Text>Project一覧表示</Text>
+    </SafeAreaView>
+  );
+};
+
+//③定数Stack内にcreateStackNavigator代入
+const Stack = createStackNavigator();
+
+//④Stack.Screen　nameでNavBarの記述　componentでcontentAreaにProjectScreen関数　をrun
 const App = () => {
   return (
     <NavigationContainer>
-      <Screen />
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={ProjectScreen} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 };
+
+const styles = StyleSheet.create({
+  HomeScreen: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
 
 export default App;
