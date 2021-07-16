4f64a623cb44287a5760a3a5ac1503fb80599089
Yuta Yotsumoto <y.2000529@gmail.com>
2021-07-16 10:00:26 +0900

    HomeScreen,ProjectScreen間でデータ(params)をpass

--- src/Screens/HomeScreen.js
+++ src/Screens/HomeScreen.js
@@ -1,23 +1,43 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {SafeAreaView, Button, Text, StyleSheet} from 'react-native';
 
 //HomeScreen関数
-const HomeScreen = ({navigation}) => {
+const HomeScreen = ({navigation, route}) => {
+  useEffect(() => {
+    if (route.params?.post) {
+      // Post updated, do something with `route.params.post`
+      // For example, send the post to the server
+    }
+  }, [route.params?.post]);
+
   return (
     <SafeAreaView style={styles.HomeScreen}>
-      <Text>Home Screen</Text>
       <Button
-        title="Head to Project"
-        onPress={() =>
-          navigation.navigate('Project', {
-            itemId: 86,
-            otherParam: 'anything you want here',
-          })
-        }
+        title="Create Project"
+        onPress={() => navigation.navigate('Project')}
       />
+      <Text style={{margin: 10}}>Project: {route.params?.post}</Text>
     </SafeAreaView>
   );
 };
+
+// //最初のHomeScreenコード
+// const HomeScreen = ({navigation}) => {
+//   return (
+//     <SafeAreaView style={styles.HomeScreen}>
+//       <Text>Home Screen</Text>
+//       <Button
+//         title="Head to Project"
+//         onPress={() =>
+//           navigation.navigate('Project', {
+//             itemId: 86,
+//             otherParam: 'anything you want here',
+//           })
+//         }
+//       />
+//     </SafeAreaView>
+//   );
+// };
 
 const styles = StyleSheet.create({
   HomeScreen: {
--- src/Screens/ProjectScreen.js
+++ src/Screens/ProjectScreen.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import {SafeAreaView, Button, Text, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {SafeAreaView, Button, StyleSheet, TextInput} from 'react-native';
 
 //ProjectScreen関数
 const ProjectScreen = ({route, navigation}) => {
-  const {itemId, otherParam} = route.params;
+  const [ProjectTitle, setProjectText] = useState('');
   return (
     <SafeAreaView style={styles.HomeScreen}>
-      <Text>Project Screen</Text>
-      <Text>itemId: {JSON.stringify(itemId)}</Text>
-      <Text>otherParam: {JSON.stringify(otherParam)}</Text>
+      <TextInput
+        multiline
+        placeholder="What's your project title ?"
+        value={ProjectTitle}
+        onChangeText={setProjectText}
+      />
       <Button
-        title="Head to Project ... again"
+        title="Done"
         onPress={() =>
-          navigation.push('Project', {itemId: Math.floor(Math.random() * 100)})
+          navigation.navigate({
+            name: 'Home',
+            params: {post: ProjectTitle},
+            merge: true,
+          })
         }
       />
-      <Button title="back Home" onPress={() => navigation.navigate('Home')} />
-      <Button title="back FirstScreen" onPress={() => navigation.popToTop()} />
-      <Button title="goBack LastScreen" onPress={() => navigation.goBack()} />
     </SafeAreaView>
   );
 };
+
+// //最初のProjectScreen関数
+// const ProjectScreen = ({route, navigation}) => {
+//   const {itemId, otherParam} = route.params;
+//   return (
+//     <SafeAreaView style={styles.HomeScreen}>
+//       <Text>Project Screen</Text>
+//       <Text>itemId: {JSON.stringify(itemId)}</Text>
+//       <Text>otherParam: {JSON.stringify(otherParam)}</Text>
+//       <Button
+//         title="Head to Project ... again"
+//         onPress={() =>
+//           navigation.push('Project', {itemId: Math.floor(Math.random() * 100)})
+//         }
+//       />
+//       <Button title="back Home" onPress={() => navigation.navigate('Home')} />
+//       <Button title="back FirstScreen" onPress={() => navigation.popToTop()} />
+//       <Button title="goBack LastScreen" onPress={() => navigation.goBack()} />
+//     </SafeAreaView>
+//   );
+// };
 
 const styles = StyleSheet.create({
   HomeScreen: {
